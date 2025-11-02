18c2617889c5e6823583193b9ce6a6f837b3aded
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-11-02 16:11:21 +0900

    adding test gnss script ......
    adding test gnss script ......

---
+++ Inubashiri_platformio_V3/esp32_receive_gnss_pio/src/main.cpp
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+#include <TinyGPSPlus.h>
+
+void displayInfo();
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Hello World on Serial, setup");
+
+  Serial2.begin(9600);
+}
+
+void loop() {
+  //delay(1000);
+  //Serial.println("Hello World on Serial, loop");
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (Serial2.available() > 0)
+  {  
+    if (gps.encode(Serial2.read()))
+    {
+      displayInfo();
+    }
+  }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+  delay(1000);
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time(UTC): "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
